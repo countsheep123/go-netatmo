074cb3b1c6e881973f586b0eeb4a6630a77c9ef4
countsheep123 <countsheep123@mbp.local>
2017-10-16 22:48:31 +0900

    fix

--- client.go
+++ client.go
@@ -1,8 +1,9 @@
 package netatmo
 
 import (
-	"encoding/json"
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/url"
 
@@ -19,28 +20,30 @@ type Config struct {
 	Password     string
 	ClientID     string
 	ClientSecret string
-	Scopes []string
+	Scopes       []string
 }
 
 type Client struct {
 	httpClient *http.Client
+	token      string
 }
 
 func NewClient(cnf *Config) (*Client, error) {
 
-	httpClient, err := auth(cnf.Username, cnf.Password, cnf.ClientID, cnf.ClientSecret, cnf.Scopes)
+	httpClient, token, err := auth(cnf.Username, cnf.Password, cnf.ClientID, cnf.ClientSecret, cnf.Scopes)
 	if err != nil {
 		return nil, err
 	}
 
 	return &Client{
 		httpClient: httpClient,
+		token:      token,
 	}, nil
 }
 
 // https://dev.netatmo.com/resources/technical/guides/authentication/clientcredentials
 // Resource Owner Password Credentials Grant
-func auth(username, password, clientID, clientSecret string, scopes []string) (*http.Client, error) {
+func auth(username, password, clientID, clientSecret string, scopes []string) (*http.Client, string, error) {
 	oauth := &oauth2.Config{
 		ClientID:     clientID,
 		ClientSecret: clientSecret,
@@ -53,15 +56,15 @@ func auth(username, password, clientID, clientSecret string, scopes []string) (*
 
 	token, err := oauth.PasswordCredentialsToken(context.TODO(), username, password)
 	if err != nil {
-		return nil, nil
+		return nil, "", nil
 	}
 
 	httpClient := oauth.Client(context.TODO(), token)
 
-	return httpClient, nil
+	return httpClient, token.AccessToken, nil
 }
 
-func (c *Client) get(u url.URL, v interface) error {
+func (c *Client) get(u url.URL, v interface{}) error {
 	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
 	if err != nil {
 		return err
@@ -73,13 +76,13 @@ func (c *Client) get(u url.URL, v interface) error {
 	}
 	defer res.Body.Close()
 
-	switch res.StatusCode{
+	switch res.StatusCode {
 	case http.StatusOK:
 		if err := json.NewDecoder(res.Body).Decode(v); err != nil {
 			return err
 		}
 		return nil
 	default:
-		return fmt.Errof("invalid request: status_code = %d", res.StatusCode)
+		return fmt.Errorf("invalid request: status_code = %d", res.StatusCode)
 	}
 }
--- getstatsionsdata.go
+++ getstatsionsdata.go
@@ -6,44 +6,80 @@ import (
 )
 
 type DashboardData struct {
-	TimeUTC     *int64   `json:"time_utc,omitempty"`
-	Temperature *float64 `json:"Temperature,omitempty"`
-	TempTrend   *string  `json:"temp_trend,omitempty"`
-	Humidity    *int64   `json:"Humidity,omitempty"`
-	DateMaxTemp *int64   `json:"date_max_temp,omitempty"`
-	DateMinTemp *int64   `json:"date_min_temp,omitempty"`
-	MinTemp     *float64 `json:"min_temp,omitempty"`
-	MaxTemp     *float64 `json:"max_temp,omitempty"`
+	AbsolutePressure *float64 `json:"AbsolutePressure,omitempty"`
+	CO2              *int64   `json:"CO2,omitempty"`
+	Humidity         *int64   `json:"Humidity,omitempty"`
+	Noise            *int64   `json:"Noise,omitempty"`
+	Pressure         *float64 `json:"Pressure,omitempty"`
+	Temperature      *float64 `json:"Temperature,omitempty"`
+	DateMaxTemp      *int64   `json:"date_max_temp,omitempty"`
+	DateMinTemp      *int64   `json:"date_min_temp,omitempty"`
+	MaxTemp          *float64 `json:"max_temp,omitempty"`
+	MinTemp          *float64 `json:"min_temp,omitempty"`
+	PressureTrend    *string  `json:"pressure_trend,omitempty"`
+	TempTrend        *string  `json:"temp_trend,omitempty"`
+	TimeUTC          *int64   `json:"time_utc,omitempty"`
 }
 
 type Module struct {
-	ID             *string        `json:"_id,omitempty"`
-	Type           *string        `json:"type,omitempty"`
-	LastMessage    *int64         `json:"last_message,omitempty"`
-	LastSeen       *int64         `json:"last_seen,omitempty"`
-	DashboardData  *DashboardData `json:"dashboard_data,omitempty"`
-	DataType       []string       `json:"data_type,omitempty"`
-	ModuleName     *string        `json:"module_name,omitempty"`
-	LastSetup      *int64         `json:"last_setup,omitempty"`
-	BatteryVP      *int64         `json:"battery_vp,omitempty"`
-	BatteryPercent *int64         `json:"battery_percent,omitempty"`
-	RfStatus       *int64         `json:"rf_status,omitempty"`
-	Firmware       *int64         `json:"firmware,omitempty"`
+	// ID             *string `json:"_id,omitempty"`
+	// LastMessage    *int64  `json:"last_message,omitempty"`
+	// LastSeen       *int64  `json:"last_seen,omitempty"`
+	// BatteryVP      *int64  `json:"battery_vp,omitempty"`
+	// BatteryPercent *int64  `json:"battery_percent,omitempty"`
+	// RfStatus       *int64  `json:"rf_status,omitempty"`
+}
+
+type Place struct {
+	City     *string   `json:"city,omitempty"`
+	Country  *string   `json:"country,omitempty"`
+	Location []float64 `json:"location,omitempty"`
+	Timezone *string   `json:"timezone,omitempty"`
 }
 
 type Device struct {
-	ID              *string   `json:"_id,omitempty"`
-	CipherID        *string   `json:"cipher_id,omitempty"`
-	LastStatusStore *int64    `json:"last_status_store,omitempty"`
-	Modules         []*Module `json:"modules,omitempty"`
+	ID              *string        `json:"_id,omitempty"`
+	CipherID        *string        `json:"cipher_id,omitempty"`
+	CO2Calibrating  *bool          `json:"co2_calibrating,omitempty"`
+	DashboardData   *DashboardData `json:"dashboard_data,omitempty"`
+	DataType        []string       `json:"data_type,omitempty"` // Temperature, CO2, Humidity, Noise, Pressure
+	DataSetup       *int64         `json:"data_setup,omitempty"`
+	Firmware        *int64         `json:"firmware,omitempty"`
+	LastSetup       *int64         `json:"last_setup,omitempty"`
+	LastStatusStore *int64         `json:"last_status_store,omitempty"`
+	LastUpgrade     *int64         `json:"last_upgrade,omitempty"`
+	ModuleName      *string        `json:"module_name,omitempty"`
+	Modules         []*Module      `json:"modules,omitempty"`
+	Place           *Place         `json:"place,omitempty"`
+	StationName     *string        `json:"station_name,omitempty"`
+	Type            *string        `json:"type,omitempty"`
+	WifiStatus      *int64         `json:"wifi_status,omitempty"`
+}
+
+type Administrative struct {
+	FeelLikeAlgo *int64  `json:"feel_like_algo,omitempty"`
+	Lang         *string `json:"lang,omitempty"`
+	Pressureunit *int64  `json:"pressureunit,omitempty"`
+	RegLocale    *string `json:"reg_locale,omitempty"`
+	Unit         *int64  `json:"unit,omitempty"`
+	Windunit     *int64  `json:"windunit,omitempty"`
+}
+
+type User struct {
+	Administrative *Administrative `json:"administrative,omitempty"`
+	Mail           *string         `json:"mail,omitempty"`
 }
 
 type Body struct {
 	Devices []*Device `json:"devices,omitempty"`
+	User    *User     `json:"user,omitempty"`
 }
 
 type StationData struct {
-	Body *Body `json:"body,omitempty"`
+	Body       *Body    `json:"body,omitempty"`
+	Status     *string  `json:"status,omitempty"`
+	TimeExec   *float64 `json:"time_exec,omitempty"`
+	TimeServer *int64   `json:"time_server,omitempty"`
 }
 
 // https://dev.netatmo.com/en-US/resources/technical/reference/weatherstation/getstationsdata
@@ -56,7 +92,7 @@ func (c *Client) Getstationsdata(deviceID string, getFavorites bool) (*StationDa
 	}
 
 	queries := url.Values{}
-	queries.Add("access_token", d.token)
+	queries.Add("access_token", c.token)
 	if len(deviceID) > 0 {
 		queries.Add("device_id", deviceID)
 	}
